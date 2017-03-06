224f6940a37601634727048963822e263489209b
scienceasdf <scienceasdf@126.com>
2017-03-06 22:58:56 +0800

    Update main.cpp

--- PROP_blocks/main.cpp
+++ PROP_blocks/main.cpp
@@ -1,27 +1,36 @@
 #include<vector>
 #include<algorithm>
 #include<iostream>
-
+#include <chrono>
 #include<string>
 
 
 #include<spline.h>
 #include<objects.h>
-
+#include<fileops.h>
 
 int main()
 {
+    auto start = std::chrono::system_clock::now();
+    motor mt1(220,.042);
+    propulsion p1(12.5,10,mt1,1.0,1.0);
+    std::vector<std::string> s1;
 
-    motor mt1(1100,.042);
-    propulsion p1(21,60,mt1,2.0,1.0);
+    p1.speed=7.0;
 
-    p1.speed=22.0;
-    p1.get_curve("PER3_15x4E.dat");
-    std::cout<<p1.prop_name<<"\n";
-    p1.run();
-    std::cout<<p1.current<<"\t"<<p1.volt<<"\t"<<p1.thru<<"\t"<<p1.rot<<"\t"<<p1.eta<<"\t"<<p1.eta_motor<<"\n";
-    p1.dynamic_run();
-    std::cout<<p1.current<<"\t"<<p1.volt<<"\t"<<p1.thru<<"\t"<<p1.rot<<"\t"<<p1.eta<<"\t"<<p1.eta_motor<<"\n";
+    getAll(s1,10,.0,.5,.6,p1);
+    int i=0;
+    std::vector<std::string>::iterator iter=s1.begin();
+    for(;iter!=s1.end();++iter){
+        std::cout<<(*iter)<<"\t"<<i<<"\n";
+        ++i;
+    }
+
+    auto end   = std::chrono::system_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout <<  "Duration time is "
+         << double(duration.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den
+         << " seconds" << std::endl;
     return 0;
 
 }
